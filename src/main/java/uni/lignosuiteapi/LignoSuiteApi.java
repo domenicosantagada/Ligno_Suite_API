50cf222628a715a386a271d9eec4f0c96cddae11
Domenico Santagada <115139470+domenicosantagada@users.noreply.github.com>
2026-02-24 17:04:47 +0100

    Bug Fix

--- src/main/java/uni/lignosuiteapi/LignoSuiteApi.java
+++ src/main/java/uni/lignosuiteapi/LignoSuiteApi.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToolFalegnameriaApiApplication {
+public class LignoSuiteApi {
 
     public static void main(String[] args) {
-        SpringApplication.run(ToolFalegnameriaApiApplication.class, args);
+        SpringApplication.run(LignoSuiteApi.class, args);
     }
 
 }
--- src/test/java/uni/lignosuiteapi/LignoSuiteApiTests.java
+++ src/test/java/uni/lignosuiteapi/LignoSuiteApiTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ToolFalegnameriaApiApplicationTests {
+class LignoSuiteApiTests {
 
     @Test
     void contextLoads() {
